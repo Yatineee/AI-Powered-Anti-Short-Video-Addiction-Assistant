b45acee28010a736cc8ea7e50530a0ad16ecbf6d
Yatineee <159103700+Yatineee@users.noreply.github.com>
2025-06-13 23:25:01 +0100

    Update middleware.py

--- web/backend/config/middleware.py
+++ web/backend/config/middleware.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_middlewares(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # ⚠️ 生产环境替换为前端域名
+        allow_origins=["http://localhost:3000"],  # ⚠️ 生产环境替换为前端域名
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
